2a01d0f9c5ee5f05388628cfe603088112380418
stzWonhoLee <wonho.lee@sundaytoz.com>
2023-11-09 18:11:32 +0900

    WIP

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const RULER_TICK_LENGTH = 10;
 const EVACUATOR_LIFETIME_SEC = 5;
 const MAG_GRAVITY = -1;
 let target = document.getElementById("main-canvas");
-var ctx = target.getContext('2d');
+let ctx = target.getContext('2d');
 let width = target.width;
 let height = target.height;
 function start_game() {
@@ -54,7 +54,6 @@ function estimate_mag(elapsed_sec) {
 let window_width_sec = 1;
 let window_height_mag = 1;
 function get_canvas_xy(sec, mag) {
-    // : {x: number, y: number, xy: [number, number]}{
     let x = MARGIN + (width - MARGIN * 2) * sec / window_width_sec;
     let y = MARGIN + (height - MARGIN * 2) * (1 - mag / window_height_mag);
     return {
@@ -62,12 +61,6 @@ function get_canvas_xy(sec, mag) {
         y,
         xy: [x, y]
     };
-    // return Object.assign([x, y] as [number, number], {x, y});
-    // return [x, y];
-    // return {
-    //     x: MARGIN + (width - MARGIN * 2) * sec / window_width_sec,
-    //     y: MARGIN + (height - MARGIN * 2) * (1 - mag / window_height_mag),
-    // }
 }
 function xline(sec, mag, length) {
     ctx.lineWidth = 1;
@@ -210,107 +203,3 @@ function repeat_tick(game) {
     }
 }
 start_game();
-let names = [];
-for (var i = 0; i < 20; ++i) {
-    names.push({
-        name: `name ${i}`,
-        xfrom: target.width / 20 * i,
-        yfrom: target.height / 20 * i,
-    });
-}
-function render() {
-    draw_lamp(target, (Date.now() % 2000) / 2000, 2);
-    requestAnimationFrame(render);
-}
-// render();
-function draw_lamp(canvas, xmax01, power) {
-    // console.log(canvas);
-    var ctx = canvas.getContext('2d');
-    let width = canvas.width;
-    let height = canvas.height;
-    // clear
-    ctx.clearRect(0, 0, width, height);
-    ctx.rect(0, 0, width, height);
-    ctx.fillStyle = 'black';
-    ctx.fill();
-    // lamp    
-    ctx.beginPath();
-    // let x = width * xmax;
-    // let y = 1 - y01;
-    ctx.moveTo(0, height);
-    const resolution = 100;
-    let x01 = 0;
-    let x = 0;
-    let points = [];
-    for (let i = 0; i < resolution && x01 <= xmax01; ++i) {
-        x01 = 1 / resolution * i;
-        x = width * x01;
-        let height01 = Math.pow(x01, power);
-        let y = height * (1 - height01);
-        points.push({ x, y });
-    }
-    for (let p of points) {
-        ctx.lineTo(p.x, p.y);
-    }
-    ctx.lineTo(x, height);
-    ctx.closePath();
-    const gradient = ctx.createLinearGradient(0, height, width, 0);
-    gradient.addColorStop(0, "black");
-    gradient.addColorStop(1, "purple");
-    ctx.fillStyle = gradient;
-    ctx.fill();
-    // lamp bound
-    // ctx.createLinearGradient()
-    let line_points0 = [];
-    let line_points1 = [];
-    for (let i = 0; i < points.length - 1; ++i) {
-        let p0 = points[i];
-        let p1 = points[i + 1];
-        let diff = { x: p1.x - p0.x, y: p1.y - p0.y };
-        line_points0.push({
-            x: p0.x + -diff.y,
-            y: p0.y + diff.x,
-        });
-        line_points1.unshift({
-            x: p0.x + diff.y,
-            y: p0.y + -diff.x,
-        });
-    }
-    ctx.beginPath();
-    ctx.moveTo(0, height);
-    for (let p of line_points0) {
-        ctx.lineTo(p.x, p.y);
-    }
-    for (let p of line_points1) {
-        ctx.lineTo(p.x, p.y);
-    }
-    ctx.closePath();
-    const line_gradient = ctx.createLinearGradient(0, height, width, 0);
-    gradient.addColorStop(0, "white");
-    gradient.addColorStop(1, "purple");
-    ctx.fillStyle = gradient;
-    ctx.fill();
-    ctx.beginPath();
-    let tip = points[points.length - 1];
-    ctx.arc(tip.x, tip.y, 10, 0, 2 * Math.PI);
-    // ctx.closePath();
-    ctx.fillStyle = 'white';
-    ctx.fill();
-    ctx.font = "10px serif";
-    for (let n of names) {
-        ctx.fillText(n.name, n.xfrom, height - n.yfrom + +x01 * height);
-    }
-    // ruler
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = "grey";
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(0, height);
-    ctx.stroke();
-    // ctx.moveTo(0, 0);
-    ctx.beginPath();
-    ctx.moveTo(0, height);
-    ctx.lineTo(width, height);
-    ctx.stroke();
-    // ctx.closePath();
-}
--- index.ts
+++ index.ts
@@ -30,7 +30,7 @@ const EVACUATOR_LIFETIME_SEC = 5;
 const MAG_GRAVITY = -1;
 
 let target = document.getElementById("main-canvas") as HTMLCanvasElement;
-var ctx: CanvasRenderingContext2D = target.getContext('2d') as CanvasRenderingContext2D;
+let ctx: CanvasRenderingContext2D = target.getContext('2d') as CanvasRenderingContext2D;
 let width = target.width;
 let height = target.height;
 
@@ -79,20 +79,13 @@ let window_width_sec = 1;
 let window_height_mag = 1;
 
 function get_canvas_xy(sec: number, mag: number) {
-// : {x: number, y: number, xy: [number, number]}{
     let x = MARGIN + (width - MARGIN * 2) * sec / window_width_sec;
     let y = MARGIN + (height - MARGIN * 2) * (1 - mag / window_height_mag);    
     return {
         x,
         y,
         xy: [x, y] as [number,  number]
-    }
-    // return Object.assign([x, y] as [number, number], {x, y});
-    // return [x, y];
-    // return {
-    //     x: MARGIN + (width - MARGIN * 2) * sec / window_width_sec,
-    //     y: MARGIN + (height - MARGIN * 2) * (1 - mag / window_height_mag),
-    // }
+    }    
 }
 
 function xline(sec: number, mag: number, length: number){
@@ -262,137 +255,3 @@ function repeat_tick(game: game){
 }
 
 start_game();
-
-
-let names: {name: string, xfrom: number, yfrom: number}[] = [];
-for(var i = 0; i < 20; ++i){
-    names.push({
-        name: `name ${i}`,
-        xfrom: target.width / 20 * i,
-        yfrom: target.height / 20 * i,
-    });
-}
-
-function render(){
-    draw_lamp(
-        target,
-        (Date.now() % 2000) / 2000,
-        2,
-    );
-
-    requestAnimationFrame(render);
-}
-
-// render();
-
-function draw_lamp(canvas: HTMLCanvasElement, xmax01: number, power: number){
-    // console.log(canvas);
-    var ctx: CanvasRenderingContext2D = canvas.getContext('2d') as CanvasRenderingContext2D;
-    let width = canvas.width;
-    let height = canvas.height;
-
-    // clear
-    ctx.clearRect(0, 0, width, height);
-    ctx.rect(0, 0, width, height);
-    ctx.fillStyle = 'black';
-    ctx.fill();
-
-    // lamp    
-    ctx.beginPath();
-    // let x = width * xmax;
-    // let y = 1 - y01;
-    ctx.moveTo(0, height);    
-
-    const resolution = 100;
-    let x01 = 0;
-    let x = 0;
-    let points: {x: number, y: number}[] = [];
-    for(let i = 0; i < resolution && x01 <= xmax01; ++i){
-        x01 = 1 / resolution * i;
-        x = width * x01;
-        let height01 = Math.pow(x01, power);
-        let y = height * (1 - height01);
-        points.push({x, y});        
-    }
-
-    for(let p of points){
-        ctx.lineTo(p.x, p.y);
-    }
-    
-    ctx.lineTo(x, height);
-    ctx.closePath();
-    
-
-    const gradient = ctx.createLinearGradient(0, height, width, 0);
-    gradient.addColorStop(0, "black");    
-    gradient.addColorStop(1, "purple");
-    ctx.fillStyle = gradient;
-    ctx.fill();
-
-    // lamp bound
-    // ctx.createLinearGradient()
-
-    let line_points0: {x: number, y: number}[] = [];
-    let line_points1: {x: number, y: number}[] = [];
-    for(let i = 0; i < points.length - 1; ++i){
-        let p0 = points[i];
-        let p1 = points[i + 1];
-        let diff = {x: p1.x - p0.x, y: p1.y - p0.y};
-        line_points0.push({
-            x: p0.x + -diff.y,
-            y: p0.y + diff.x,
-        });
-        line_points1.unshift({
-            x: p0.x + diff.y,
-            y: p0.y + -diff.x,
-        });
-    }
-
-    ctx.beginPath();    
-    ctx.moveTo(0, height);    
-    for(let p of line_points0){
-        ctx.lineTo(p.x, p.y);
-    }
-    for(let p of line_points1){
-        ctx.lineTo(p.x, p.y);
-    }
-    ctx.closePath();
-
-    const line_gradient = ctx.createLinearGradient(0, height, width, 0);
-    gradient.addColorStop(0, "white");    
-    gradient.addColorStop(1, "purple");
-    ctx.fillStyle = gradient;
-    ctx.fill();
-
-    ctx.beginPath();
-    let tip = points[points.length - 1];
-    ctx.arc(tip.x, tip.y, 10, 0, 2 * Math.PI);    
-    // ctx.closePath();
-    ctx.fillStyle = 'white';
-    ctx.fill();
-
-    ctx.font = "10px serif";
-    
-
-    for(let n of names){
-        ctx.fillText(n.name, n.xfrom, height - n.yfrom +  + x01 * height);
-    }
-
-    // ruler
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = "grey";    
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(0, height);
-    ctx.stroke();
-
-    // ctx.moveTo(0, 0);
-    ctx.beginPath();
-    ctx.moveTo(0, height);
-    ctx.lineTo(width, height);
-    ctx.stroke();
-    
-    
-    // ctx.closePath();
-    
-}
